b049c79e1f9c41782036ea511fd95623585e3ed1
Lucas Kurata <lucas.kurata@hotmail.com>
2019-10-19 16:24:52 -0300

    Adciona CRUD para todas as telas

--- PHP/consultandoProduto.php
+++ PHP/consultandoProduto.php
@@ -5,14 +5,20 @@ require "conexao.php";
 ?>
 <html>
  <head>
- <link href="estilos.css" rel="stylesheet" type="text/css">
- <title>Resultado da pesquisa</title>
- </head>
- <body>
- <a href = "../HTML/cadastrarProduto.php">Cadastrar novo produto</a>
+ <link href="../CSS/formataTabelaCrud.css" rel="stylesheet" type="text/css">
+ <title>Consultando Produtos</title>
+</head>
+<body>
+<h1>Consultar Produtos</h1>
+<div id = "navbar">
+  <a href="../HTML/menu.html">Voltar para Menu</a>
+  <a href="../HTML/index.html">Sair do sistema</a>
+</div>
+<a class = "navbaresquerda" href= "../HTML/cadastrarProduto.php">Novo Produto</a>
 
+ <hr/>
  <form action = "editarProduto.php" method = "get">
- <table border="1" style='width:80%'>
+ <table>
   <thead>
     <tr>
       <th>ID</th>
@@ -25,16 +31,27 @@ require "conexao.php";
       <th>Fornecedor</th>
       <th>Categoria</th>
       <th>Cor</th>
-      <th>Excluir</th>
       <th>Alterar</th>
+      <th>Excluir</th>
     </tr>
   </thead>
   <tbody>
 <?php
 
 // Executando consulta SQL
-$query = 'SELECT codProduto, nome_produto, marca, numeracao, quantidade, 
-    valor_custo, valor_venda, fornecedor, categoria_produto, cor FROM produtos';
+$query = 'SELECT 
+          codProduto, 
+          nome_produto, 
+          marca, 
+          numeracao, 
+          quantidade, 
+          CONCAT("R$ ", valor_custo) AS valor_custo, 
+          CONCAT("R$ ", valor_venda) AS valor_venda, 
+          fornecedor, 
+          categoria_produto, 
+          cor 
+          FROM produtos
+          ORDER BY codProduto';
 $result = mysqli_query($con, $query) or die('Query failed: ' . mysql_error());
 
 // Imprimindo resultados em HTML
@@ -55,8 +72,10 @@ while ($registro = mysqli_fetch_array($result)){
     <td><?php echo $registro['fornecedor']; ?></td>
     <td><?php echo $registro['categoria_produto']; ?></td>
     <td><?php echo $registro['cor']; ?></td>
-    <td><a href = "deletarProduto.php?id=<?php echo $registro['codProduto'] ?>">Excluir</a></td>
-    <td><a href = "editarProduto.php?id=<?php echo $registro['codProduto'] ?>">Editar</a></td>
+    <td id = "excluir"><a href = "editarProduto.php?id=<?php echo $registro['codProduto'] ?>">Alterar</a></td>
+    <td id = "editar"><a href="deletarProduto.php?id=<?php echo $registro['codProduto'] ?>" 
+    onclick="return confirm('Tem certeza que deseja excluir este registro?')">Excluir</a></td>
   </tr>
     <?php } ?>
 </form>
+
